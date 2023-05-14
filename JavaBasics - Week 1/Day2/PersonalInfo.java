796ebf5d2d415d113692f42564fbc97ef6215bf1
Lily Carter <lilycarter54@gmail.com>
2023-05-14 13:57:26 +0100

    Add day2 files

---
+++ JavaBasics - Week 1/Day2/PersonalInfo.java
@@ -0,0 +1,8 @@
+class PersonalInfo{
+	public static void main(String args[]){
+		System.out.println("My name is Lily Carter");
+		System.out.println("Address: 303 Queensbridge Road, London, E8 3LX");
+		System.out.println("Phone number: 07447154469");
+	}
+}
+	
